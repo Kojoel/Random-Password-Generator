a58ea56f3bbe0f0b0821190af531c06d24dc4944
Kojoel <nanaabruquah1s@gmail.com>
2024-07-15 04:17:17 -0700

    Created assets and card component

---
+++ RPG/src/app/pg-card/pg-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pg-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './pg-card.component.html',
+  styleUrl: './pg-card.component.scss'
+})
+export class PgCardComponent {
+
+}
