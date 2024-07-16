49624f794a4b81e1f070c2c239a6b0d9acd77749
Kojoel <nanaabruquah1s@gmail.com>
2024-07-16 03:34:03 -0700

    Implemented generate password feature

--- RPG/src/app/pg-card/pg-card.component.ts
+++ RPG/src/app/pg-card/pg-card.component.ts
@@ -9,29 +9,62 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './pg-card.component.scss'
 })
 export class PgCardComponent {
+  private readonly lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
+  private readonly uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  private readonly digitChars = '0123456789';
+  private readonly specialChars = '!@#$%^&*()-_=+[{]}\\|;:\'",<.>/?';
+
   generatedPw: string = "SEL23X832";
   charLength: number = 10;
   progressBar: number = 10;
+  upperCaseIncluded: boolean = true;
+  lowerCaseIncluded: boolean = true;
+  numbersIncluded: boolean = true;
+  symbolsIncluded: boolean = false;
 
-  // ngOnInit() {
-  //   this.progressBar= document.querySelector('.progressBar');
-  //   this.charLength = this.progressBar.value;
-  // }
+  generatePassword(): void {
+    let charSet = '';
+    if (this.upperCaseIncluded) charSet += this.uppercaseChars;
+    if (this.lowerCaseIncluded) charSet += this.lowercaseChars;
+    if (this.numbersIncluded) charSet += this.digitChars;
+    if (this.symbolsIncluded) charSet += this.specialChars;
 
-  // charLength: any;
-  // progressBar: any;
-  // isChecked = true;
-  // upperCaseCheck: any;
+    if (charSet.length === 0) {
+      throw new Error('Please select at least one character set option');
+    }
 
-  // ngOnInit() {
-  //   this.charLength = document.querySelector('.char-length');
-  //   this.progressBar= document.querySelector('.progressBar');
-  //   this.upperCaseCheck = document.querySelector('#upperCaseIncluded');
-  // }
+    let password = '';
+    for (let i = 0; i < this.charLength; i++) {
+      const randomIndex = Math.floor(Math.random() * charSet.length);
+      password += charSet[randomIndex];
+    }
+    this.generatedPw = password;
+    // return password;
+  }
 
-  // getProgressValue = () => {
-  //   this.charLength.textContent = this.progressBar.value;
-  //   // console.log(this.charLength.textContent);
-  //   console.log(this.upperCaseCheck);
-  // }
+
+  ratePasswordStrength(password: string): string {
+    const hasLowercase = /[a-z]/.test(password);
+    const hasUppercase = /[A-Z]/.test(password);
+    const hasDigits = /\d/.test(password);
+    const hasSpecialChars = /[!@#$%^&*()-_=+[{]}\\\|;:\'\",<.>\/\?]/.test(password);
+
+    const typesCount = [hasLowercase, hasUppercase, hasDigits, hasSpecialChars].filter(Boolean).length;
+
+    if (password.length < 8) {
+      return 'Too Weak';
+    } else if (password.length >= 8 && typesCount === 1) {
+      return 'Weak';
+    } else if (password.length >= 8 && typesCount >= 2) {
+      return 'Medium';
+    } else if (password.length >= 12 && typesCount >= 3) {
+      return 'Strong';
+    } else {
+      return 'Medium'; // Default to Medium if none of the above conditions are met
+    }
+  }
+
+  
+  
+
 }
