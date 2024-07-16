a2d049df1524c6469837c001a9fba2aa04076af1
Kojoel <nanaabruquah1s@gmail.com>
2024-07-16 08:33:43 +0000

    Dynamic Slider using ngModel

--- RPG/src/app/pg-card/pg-card.component.ts
+++ RPG/src/app/pg-card/pg-card.component.ts
@@ -1,26 +1,37 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-pg-card',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './pg-card.component.html',
   styleUrl: './pg-card.component.scss'
 })
 export class PgCardComponent {
-  charLength: any;
-  progressBar: any;
-  isChecked = true;
-  upperCaseCheck: any;
+  generatedPw: string = "SEL23X832";
+  charLength: number = 10;
+  progressBar: number = 10;
 
-  ngOnInit() {
-    this.charLength = document.querySelector('.char-length');
-    this.progressBar= document.querySelector('.progressBar');
-    this.upperCaseCheck = document.querySelector('#upperCaseIncluded');
-  }
+  // ngOnInit() {
+  //   this.progressBar= document.querySelector('.progressBar');
+  //   this.charLength = this.progressBar.value;
+  // }
 
-  getProgressValue = () => {
-    this.charLength.textContent = this.progressBar.value;
-    console.log(this.charLength.textContent);
-  }
+  // charLength: any;
+  // progressBar: any;
+  // isChecked = true;
+  // upperCaseCheck: any;
+
+  // ngOnInit() {
+  //   this.charLength = document.querySelector('.char-length');
+  //   this.progressBar= document.querySelector('.progressBar');
+  //   this.upperCaseCheck = document.querySelector('#upperCaseIncluded');
+  // }
+
+  // getProgressValue = () => {
+  //   this.charLength.textContent = this.progressBar.value;
+  //   // console.log(this.charLength.textContent);
+  //   console.log(this.upperCaseCheck);
+  // }
 }
